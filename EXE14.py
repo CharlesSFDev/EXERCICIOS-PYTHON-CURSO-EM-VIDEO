d48fcb4a96573ed90c34d8cfd3f347059e08d75b
Charles Fortunato <97842186+CharlesSFDev@users.noreply.github.com>
2022-01-18 12:00:52 -0300

    Delete EXE14.py

--- EXE14.py
+++
@@ -1,7 +0,0 @@
-#CÓDIGO QUE RECEBE UM VALOR DE TEMPERATURA EM CELSIUS E A CONVERTE PARA FAHRENHEIT
-
-tempCelsius = float(input('Digite o valor da temperatura em º Celsius: '))
-
-tempFahrenheit = ((9/5)*tempCelsius) + 32
-
-print('O valor digitado pelo usuário, equivalente a {}ºC, em ºF é igual a {}'.format(tempCelsius,tempFahrenheit))
