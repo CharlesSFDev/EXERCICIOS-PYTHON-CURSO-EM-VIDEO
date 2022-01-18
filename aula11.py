29bdd5c015a558b32e07d9cf557b7f89ffbbd81a
CharlesSFDev <charles.cdjsf@gmail.com>
2022-01-18 19:38:42 -0300

    adding aula11

---
+++ aula11.py
@@ -0,0 +1,11 @@
+# CÓDIGOS DE CORES EM PYTHON
+
+print('\033[31mOlá, mundo!!')  #letras em vermelho
+print('\033[31;43mOlá, mundo!!') #letras em vermelho, fundo amarelo
+print('\033[1;31mOlá, mundo!!') #letras em vermelho negrito, fundo amarelo
+print('\033[1;31mOlá, mundo!!\033[m') #fundo amarelo só na mensagem
+print('\033[4;30;45mOlá, mundo!!\033[m') #letra preta, sublinhada, com fundo lilas
+print('\033[39mOlá, mundo!!\033[m') #letra branca
+print('\033[7;39mOlá, mundo!!\033[m')#letra preta, fundo branco
+
+print(19%2)
