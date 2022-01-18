3e14a38bb993a4853ac493cffec35436d8e2ef3c
Charles Fortunato <97842186+CharlesSFDev@users.noreply.github.com>
2022-01-18 12:01:08 -0300

    Delete EXE16.py

--- EXE16.py
+++
@@ -1,9 +0,0 @@
-#CÓDIGO QUE RECEBE DO USUÁRIO UM NÚMERO REAL DECIMA E IMPRIME APENAS A PARTE INTEIRA DO MESMO
-
-import math
-
-num = float(input('Digite um número qualquer: '))
-
-inteiro = math.trunc(num)
-
-print('A parte inteira do número {} é: {}.'.format(num,inteiro))
